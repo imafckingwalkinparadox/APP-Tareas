bc3c2ac907f0c78f49db3ed507bd964d256dfc43
Fluffy McDork <hdvillatoro@gmial.com>
2025-02-07 11:40:07 -0600

    se hacen cambios en la rama tarea 	-tarea.js 	-tarea.css 	-se agrega data.js

---
+++ Componentes/Tarea/data.js
@@ -0,0 +1,25 @@
+// data.js
+export const tareas = [
+    {
+      categoria: "Design",
+      tareas: [
+        { nombre: "Creat Icons for a dashboard", completada: false },
+        { nombre: "Prepare a design presentation", completada: false },
+      ],
+    },
+    {
+      categoria: "Personal",
+      tareas: [
+        { nombre: "Stretch for 15 min", completada: false },
+        { nombre: "Plan your meal", completada: false },
+        { nombre: "Review daily goals before sleeping", completada: false },
+      ],
+    },
+    {
+      categoria: "Personal",
+      tareas: [
+        { nombre: "Water indoor plants", completada: false },
+      ],
+    },
+  ];
+  
